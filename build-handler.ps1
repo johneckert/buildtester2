9c5d634720fab6ff51b0b50da69ffae34fd8dedc
johneckert <johnteckert@gmail.com>
2020-02-11 15:02:56 -0500

    new branch

--- build-handler.ps1
+++ build-handler.ps1
@@ -2,4 +2,4 @@ write-host "I am in build-handler.ps1 in buildtester" -ForegroundColor Yellow
 
 New-Item -Path . -Name "workspace" -ItemType "directory"
 
-New-Item -Path './workspace' -Name "built2"-ItemType "file" -Value "From Build Tester"
+New-Item -Path './workspace' -Name "built2dada"-ItemType "file" -Value "From Build Tester"
